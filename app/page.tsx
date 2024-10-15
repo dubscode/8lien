080878366808334566306d7c896784017dc6daf5
Daniel Wise <io.dwise@gmail.com>
2024-10-14 18:57:48 -0700

    feat(app): implement feature flag. add flag for paginated monster queries

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import { Banner } from '@/components/banner';
 import MonsterGallery from '@/components/monsters/monster-gallery';
+import MonsterGalleryPaginated from '@/components/monsters/monster-gallery-paginated';
 import { Navbar } from '@/components/navbar';
+import { paginateMonstersFlag } from '@/flags';
 
-export default function Home() {
+export default async function Home() {
+  const paginatedMonsters = await paginateMonstersFlag();
+
   return (
     <div className='min-h-screen bg-background'>
       <Navbar />
       <main className='mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8'>
         <Banner />
-        <MonsterGallery />
+        {paginatedMonsters ? <MonsterGalleryPaginated /> : <MonsterGallery />}
       </main>
     </div>
   );
